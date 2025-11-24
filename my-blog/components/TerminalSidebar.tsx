be96f1d14efe377d525170b85ce2d88792584e4c
nghiatd-29581 <gackiemvi3m@gmail.com>
2025-11-24 15:10:23 +0700

    fix

--- my-blog/components/TerminalSidebar.tsx
+++ my-blog/components/TerminalSidebar.tsx
@@ -1,3 +1,7 @@
+
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 // components/TerminalSidebar.tsx (chỉ sửa phần này thôi)
 export function TerminalSidebar({ 
   book, 
