7cebc0555e94c07707f4b1a98063902f790c4fad
nghiatd-29581 <gackiemvi3m@gmail.com>
2026-02-04 17:32:09 +0700

    Add 'About Me' link to Header component
    Added a new 'About Me' link to the header.

--- my-blog/components/Header.tsx
+++ my-blog/components/Header.tsx
@@ -1,43 +1,19 @@
 "use client";
 
-
-
 import { useTheme } from "next-themes";
-
 import Link from "next/link";
 
-
-
 export default function Header() {
-
   const { theme, setTheme } = useTheme();
-
-
-
   return (
-
     <header className="flex justify-between items-center p-4 bg-sidebar shadow-md">
-
       <Link href="/" className="text-xl font-bold text-foreground">
-
         Blog của Nghĩa, Trần Đắc
-
       </Link>
-
-      <button
-
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-
-        className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
-
-      >
-
-         {theme === "dark" ? "Light" : "Dark"} 
-
-      </button>
-
+      
+      <Link href="/my-info" className="text-xl font-bold text-foreground">
+        Về tôi
+      </Link>
     </header>
-
   );
-
 }
